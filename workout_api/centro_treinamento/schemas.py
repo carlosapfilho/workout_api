e9083558045007919b52ffed05c1dad7697b3ebb
carlosapfilho <carlosapfilho@gmail.com>
2024-07-15 13:31:06 -0300

    feat: add schemas

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -5,9 +5,9 @@ from workout_api.contrib.schemas import BaseSchema
 
 
 class CentroTreinamentoIn(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='Toca da Raposa', max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, Q02', max_length=60)]
-    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', example='Marcos', max_length=30)]
+    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', example='Pedrinho', max_length=30)]
 
 
 class CentroTreinamentoAtleta(BaseSchema):
